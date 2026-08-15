c3ce7bc645ca5ac404385e9d5edf4f2236be0d4f
Kenny <knkadu_antwi001@st.ug.edu.gh>
2026-08-15 14:23:00 +0000

    test: cover closes trailer for issue 99

--- gateway/src/github_sidecar.test.ts
+++ gateway/src/github_sidecar.test.ts
@@ -470,3 +470,9 @@ describe("sidecar rendering cases", () => {
     expect(closesIssueLine('https://github.com/o/r/issues/21')).toBe('Closes #21');
   });
 });
+
+describe("sidecar rendering", () => {
+  it("handles closes trailer for issue 99", () => {
+    expect(closesIssueLine('https://github.com/o/r/issues/99')).toBe('Closes #99');
+  });
+});
