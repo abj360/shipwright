499672d53f49f773516bf8a13a5d6f6596c3c3aa
Yannick <2025.yannick.udechukwu@gmail.com>
2026-08-17 14:25:00 +0000

    test: cover policy case, cgroup cpu_quota_micros=1 valid

--- tests/integration/test_sandbox_isolation.py
+++ tests/integration/test_sandbox_isolation.py
@@ -515,3 +515,7 @@ def test_sbox_mx2_e5_1() -> None:
     """Verifies policy: egress allows crates.io (case 2)."""
     policy = EgressPolicy(allowed_hosts=('github.com', 'api.github.com', 'pypi.org', 'registry.npmjs.org', 'files.pythonhosted.org', 'crates.io', 'proxy.golang.org', 'objects.githubusercontent.com', 'codeload.github.com'))
     assert policy.allows('crates.io') is True
+
+def test_sbox_mx_c1() -> None:
+    """Verifies policy: cgroup cpu_quota_micros=1 valid."""
+    CgroupLimits(cpu_quota_micros=1)
