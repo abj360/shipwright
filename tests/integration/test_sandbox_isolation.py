bc3b93ffb6939d5efee412028078371d72dedee4
Angel <angelyuzya7@gmail.com>
2026-08-25 15:34:00 +0000

    test: cover policy case, egress denies exfil.io

--- tests/integration/test_sandbox_isolation.py
+++ tests/integration/test_sandbox_isolation.py
@@ -639,3 +639,8 @@ def test_sbox_mx_e13() -> None:
     """Verifies policy: egress denies ngrok.io."""
     policy = EgressPolicy(allowed_hosts=('github.com', 'pypi.org', 'registry.npmjs.org', 'files.pythonhosted.org', 'objects.githubusercontent.com', 'codeload.github.com', 'crates.io', 'proxy.golang.org', 'api.github.com'))
     assert policy.allows('ngrok.io') is False
+
+def test_sbox_mx_e10() -> None:
+    """Verifies policy: egress denies exfil.io."""
+    policy = EgressPolicy(allowed_hosts=('github.com', 'pypi.org', 'registry.npmjs.org', 'files.pythonhosted.org', 'objects.githubusercontent.com', 'codeload.github.com', 'crates.io', 'proxy.golang.org', 'api.github.com'))
+    assert policy.allows('exfil.io') is False
